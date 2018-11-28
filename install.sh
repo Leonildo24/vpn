0fa7630a79d5258397fd26ea228928393ddc4d1e
Leonildo24 <45412014+Leonildo24@users.noreply.github.com>
2018-11-27 23:34:30 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo instalando
+rm -rf install.sh
+apt-get update -y
+apt-get upgrade -y
+apt-get install wget -y
+#cd /data/data/com.termux/files/usr/bin
+rm -rf /data/data/com.termux/files/usr/bin/routervpn
+rm -rf /data/data/com.termux/files/usr/bin/routec.cpython-37.pyc
+mv routervpn /data/data/com.termux/files/usr/bin
+mv routec.cpython-37.pyc /data/data/com.termux/files/usr/bin
+apt-get install termux-api -y
+#cp routec.cpython-37.pyc /data/data/com.termux/files/usr/bin
+chmod -R 777 /data/data/com.termux/files/usr/bin/*
+echo abrindo 
+sleep 2
+routervpn
